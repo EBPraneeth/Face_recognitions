0b2fcef5c3f7645d4a0380db5e80700fe9a46565
EBPraneeth <praneethebeyonds@gmail.com>
2024-02-22 16:39:41 +0530

    add data set url and experiment notebooks

---
+++ main.py
@@ -0,0 +1,4 @@
+from face_recognitions import logger
+
+
+logger.info("Welcome to custom log")
--- template.py
+++ template.py
@@ -4,18 +4,18 @@ import logging
 
 logging.basicConfig(level=logging.INFO,format='[%(asctime)s]:%(message)s:')
 
-project_name='face_recognition'
+project_name='face_recognitions'
 
 list_of_files=[
     '.github/workflows/.gitkeep',
-    f'src/{project_name}/__int__.py',
-    f'src/{project_name}/components/__int__.py',
-    f'src/{project_name}/utils/__int__.py',
-    f'src/{project_name}/config/__int__.py',
+    f'src/{project_name}/__init__.py',
+    f'src/{project_name}/components/__init__.py',
+    f'src/{project_name}/utils/__init__.py',
+    f'src/{project_name}/config/__init__.py',
     f'src/{project_name}/config/configuration.py',
-    f'src/{project_name}/pipeline/__int__.py',
-    f'src/{project_name}/entity/__int__.py',
-    f'src/{project_name}/constants/__int__.py',
+    f'src/{project_name}/pipeline/__init__.py',
+    f'src/{project_name}/entity/__init__.py',
+    f'src/{project_name}/constants/__init__.py',
     'config/config.yaml',
     'dvc.yaml',
     'params.yaml',
