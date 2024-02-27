08f76a215d1309c4eaa974c961b8d5e27738a5a4
EBPraneeth <praneethebeyonds@gmail.com>
2024-02-27 16:29:59 +0530

    update training model note book

--- src/face_recognitions/config/configuration.py
+++ src/face_recognitions/config/configuration.py
@@ -1,6 +1,6 @@
 from face_recognitions.constants import *
 from face_recognitions.utils.common import read_yaml,create_directories
-from face_recognitions.entity.config_entity import DataIngestionConfig
+from face_recognitions.entity.config_entity import DataIngestionConfig,Train_Representation_Model_Config
 
 class ConfigurationManager:
     def __init__(self,
@@ -23,3 +23,25 @@ class ConfigurationManager:
             unzip_dir=config.unzip_dir
         )
         return data_ingestion_config
+    
+    def get_train_representation_model_config(self)->Train_Representation_Model_Config:
+        config=self.config.train_model
+        params=self.params
+        print(config)
+        create_directories([config.model_root_dir])
+
+        train_representation_model_config=Train_Representation_Model_Config(
+            model_root_dir=config.model_root_dir,
+            represent_file= config.represent_file,
+            train_faces_dir=config.train_faces_dir,
+            model_name= params.MODEL_NAME,
+            face_detector_backend=params.FACE_DETECTOR_BACKEND,
+            target_size=params.TARGET_SIZE, 
+            align=params.ALIGN,
+            enforce_detection=params.ENFORCE_DETECTION,
+            expand_percentage=params.EXPAND_PERCENTAGE,
+            normalization=params.NORMALIZATION,
+            silent=params.SILENT
+        )
+
+        return train_representation_model_config
--- src/face_recognitions/entity/config_entity.py
+++ src/face_recognitions/entity/config_entity.py
@@ -7,3 +7,17 @@ class DataIngestionConfig:
     source_url:str
     zip_data_File:Path
     unzip_dir:Path
+
+@dataclass(frozen=True)
+class Train_Representation_Model_Config:
+    model_root_dir: Path
+    represent_file:Path
+    train_faces_dir:Path
+    model_name: str
+    face_detector_backend: str
+    target_size: tuple
+    align: bool
+    enforce_detection: bool
+    expand_percentage: int
+    normalization:str
+    silent: bool
